6e3c00e83698dc35b15b6b89f6f4eefdd24ccac1
Kercon <kercondark@gmail.com>
2017-04-14 15:52:13 +0200

    Basic Files

---
+++ Basic.py
@@ -0,0 +1,49 @@
+import random
+import sys
+
+
+def ExitFunction():
+    print('Bye Bye')
+    sys.exit()
+
+
+def Printseperators():
+    print('print(\'cats\',\'dogs\',\'mice\')')
+    print('cats', 'dogs', 'mice')
+    print()
+    print('print(\'cats\',\'dogs\',\'mice\',sep=\',\')')
+    print('cats', 'dogs', 'mice', sep=',')
+    print()
+
+
+def Lists():
+    number = 1
+    names = []
+    print('input names, empty name will end loop')
+    while True:
+        print('Name', number, ':', end='')
+        name = input()
+        if name == '':
+            break
+        names += [name]
+        number += 1
+    print('Inputed Names')
+    for name in names:
+        print(name, end=' ')
+    print()
+
+
+def FunSelector(text):
+    if text == 'exit':
+        ExitFunction()
+    if text == 'printsep':
+        Printseperators()
+    if text == 'list':
+        Lists()
+
+while True:
+    print('Type printsep to Print seperators samples')
+    print('Type list to Print lists samples')
+    print('Type exit to exit')
+    response = input()
+    FunSelector(response)
